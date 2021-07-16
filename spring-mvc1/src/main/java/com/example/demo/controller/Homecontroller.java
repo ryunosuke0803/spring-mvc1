8ad36c348cc9625bbc4c9f15c6ff9184c7c308df
ryunosuke0803 <earnest_to_ryunosuke@yahoo.co.jp>
2021-07-16 20:51:11 +0900

    spring-mvc1 first commit

---
+++ spring-mvc1/src/main/java/com/example/demo/controller/Homecontroller.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.model.User;
+
+@Controller
+public class Homecontroller {
+	
+	//@ModelAttribute モデル属性に結びつける
+	
+	@GetMapping("/form")
+	private String readForm(@ModelAttribute User user) {
+		return "form";
+	}
+	
+	@PostMapping("/form")
+	private String confirm(@ModelAttribute User user) {
+		return "confirm";
+	}
+}
---
+++ spring-mvc1/src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,11 @@
+package com.example.demo.model;
+
+import lombok.Data;
+
+//Getter,Setterのコードを自動生成
+@Data
+public class User {
+	private String name;
+	private String email;
+	private String age;
+}
